a9983003df239c76580ab94086fe0c664a30bc28
BreyBatten <47111428+BreyBatten@users.noreply.github.com>
2020-05-01 20:51:07 -0400

    completed all tasks through day 4.

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -8,29 +8,108 @@ class CPU:
     def __init__(self):
         """Construct a new CPU."""
         self.reg = [0] * 8
-        self.ram = []
         self.pc = 0
+        self.ir = None
+        self.ram = [0] * 256
+        self.sp = 256
+        self.commands = {
+            0b00000001: self.hlt,
+            0b10000010: self.ldi,
+            0b01000111: self.prn,
+            0b10100010: self.mul,
+            0b01000101: self.push,
+            0b01000110: self.pop,
+            0b01010000: self.call,
+            0b00010001: self.ret,
+            0b10100000: self.add
+        }
 
-    def load(self):
+    def ram_read(self, address):
+        return self.ram[address]
+
+    def ram_write(self, value, address):
+        self.ram[address] = value
+
+    def hlt(self, operand_a, operand_b):
+        return (0, False)
+
+    def ldi(self, operand_a, operand_b):
+        self.reg[operand_a] = operand_b
+        return (3, True)
+
+    def prn(self, operand_a, operand_b):
+        print(self.reg[operand_a])
+        return(2, True)
+
+    def mul(self, operand_a, operand_b):
+        self.alu("MUL", operand_a, operand_b)
+        return (3, True)
+    
+    def add(self, operand_a, operand_b):
+        self.alu("ADD", operand_a, operand_b)
+        return (3, True)
+
+    def push(self, operand_a, operand_b):
+        #reg_address = self.ram[self.pc + 1]
+        self.sp -= 1
+        value = self.reg[operand_a]
+        self.ram[self.sp] = value
+        return (2, True)
+    
+    def pop(self, operand_a, operand_b):
+        pop_value = self.ram[self.sp]
+        reg_address = operand_a
+        self.reg[reg_address] = pop_value
+        self.sp += 1
+        return (2, True)
+
+    def call(self, operand_a, operand_b):
+        next_address = self.pc + 2
+        self.reg[2] = next_address
+        self.push(2, None)
+
+        routine_address = self.reg[operand_a]
+        self.pc = routine_address
+        return (0, True)
+    
+    def ret(self, *args):
+        self.pop(2, None)
+        next_address = self.reg[2]
+        self.pc = next_address
+        return (0, True)
+
+    def load(self, program):
         """Load a program into memory."""
 
         address = 0
 
         # For now, we've just hardcoded a program:
 
-        program = [
+        #program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        #    0b10000010, # LDI R0,8
+        #    0b00000000,
+        #    0b00001000,
+        #    0b01000111, # PRN R0
+        #    0b00000000,
+        #    0b00000001, # HLT
+        #]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        #for instruction in program:
+        #    self.ram[address] = instruction
+        #    address += 1
+        with open(program) as f:
+            for line in f:
+                comment_split = line.split('#')
+                num = comment_split[0].strip()
+
+                try:
+                    self.ram_write(int(num, 2), address)
+                    address += 1
+                except ValueError:
+                    print("Value Error")
+                    pass
+
 
 
     def alu(self, op, reg_a, reg_b):
@@ -39,6 +118,8 @@ class CPU:
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         #elif op == "SUB": etc
+        elif op == "MUL":
+            self.reg[reg_a] = (self.reg[reg_a] * self.reg[reg_b])
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -62,12 +143,22 @@ class CPU:
 
         print()
 
-    def ram_read(self, mar):
-        return getattr(self.ram[mar])
-
-    def ram_write(self, mar, mdr, addr):
-        setattr(self.ram[mar], mdr)
-
     def run(self):
         """Run the CPU."""
-        pass
+        running = True
+        
+        while running:
+            instruction_register = self.ram_read(self.pc)
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+
+            try:
+                f = self.commands[instruction_register]
+                # print(f)
+                operation_op = f(operand_a, operand_b)
+                running = operation_op[1]
+                self.pc += operation_op[0]
+
+            except:
+                print(f"Error: Instruction {instruction_register} not found")
+                sys.exit(1)
